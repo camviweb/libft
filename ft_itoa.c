2bbb3a4873584def9c9ed759ddf31478313c08d7
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-09 15:40:22 +0100

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,53 +6,64 @@
 /*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 12:28:28 by canguyen          #+#    #+#             */
-/*   Updated: 2024/12/06 14:51:10 by canguyen         ###   ########.fr       */
+/*   Updated: 2024/12/09 15:37:18 by canguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static	int	get_len(int n)
+static  int     get_len(int n)
 {
-	int	len;
+        int     len;
 
-	len = 0;
-	if (n < 0)
-	{
-		len++;
-		n = -n;
-	}
-	while (n > 0)
-	{
-		n = n / 10;
-		len++;
-	}
-	return (len);
+        len = 0;
+        if (n < 0)
+        {
+                len++;
+                n = -n;
+        }
+        while (n > 0)
+        {
+                n = n / 10;
+                len++;
+        }
+        if (n == 0)
+                len = 1;
+        len++;
+        return (len);
 }
 
-char	*ft_itoa(int n)
+static  char    *ft_start(int n)
 {
-	char	*new;
-	int		len;
-	int		i;
+        char    *new;
 
-	i = 0;
-	len = get_len(n);
-	new = malloc (sizeof * new * len);
-	if (new == NULL)
-		return (NULL);
-	if (n < 0)
-	{
-		n = -n;
-		new[i] = '-';
-		i++;
-	}
-	while (n > 0)
-	{
-		new[i] = n % 10 + '0';
-		n = n / 10;
-		i++;
-	}
-	return (new);
+        new = malloc(sizeof * new * (get_len(n)));
+        if (new == NULL)
+                return (NULL);
+        return (new);
+}
+
+char    *ft_itoa(int n)
+{
+        char    *new;
+        int             len;
+
+        len = get_len(n);
+        new = ft_start(n);
+        if (n == 0)
+                new[0] = '0';
+        if (n < 0)
+        {
+                new[0] = '-';
+                n = -n;
+        }
+        while (n > 0)
+        {
+                new[len - 2] = n % 10 + '0';
+                n = n / 10;
+                len--;
+        }
+        new[get_len(n) - 1] = '\0';
+        return (new);
 }
