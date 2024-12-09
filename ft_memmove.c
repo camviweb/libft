3a4cae366b9cc027580299cf10bde36cc37549cb
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-09 16:57:17 +0100

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,24 +6,35 @@
 /*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 10:36:52 by canguyen          #+#    #+#             */
-/*   Updated: 2024/12/06 12:54:09 by canguyen         ###   ########.fr       */
+/*   Updated: 2024/12/09 16:54:40 by canguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
 
-void	*ft_memmove(void *dst, const void *src, size_t size)
+void    *ft_memmove(void *dst, const void *src, size_t size)
 {
-	size_t	i;
-	char	*temp;
+        size_t  i;
 
-	i = 0;
-	temp = (char *)src;
-	while (i < size)
-	{
-		((char *)dst)[i] = temp[i];
-		i++;
-	}
-	return (dst);
+        i = 0;
+        if (dst > src)
+        {
+                i = size - 1;
+                while (i >= 0)
+                {
+                        ((char *)dst)[i] = ((char *)src)[i];
+                        i--;
+                }
+        }
+        else
+        {
+                while (i < size)
+                {
+                        ((char *)dst)[i] = ((char *)src)[i];
+                        i++;
+                }
+        }
+        return (dst);
 }
+~
