8bd0d2f0eb47f519afe366bd00d4ac1c467da992
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-09 17:38:30 +0100

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,24 +6,26 @@
 /*   By: canguyen <canguyen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 11:53:26 by canguyen          #+#    #+#             */
-/*   Updated: 2024/12/06 13:16:21 by canguyen         ###   ########.fr       */
+/*   Updated: 2024/12/09 17:35:23 by canguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strchr(const char *str, int c)
+char    *ft_strchr(const char *str, int c)
 {
-	int	i;
+        int     i;
 
-	i = 0;
-	while (i < ft_strlen((char *)str) + 1)
-	{
-		if (str[i] == c)
-		{
-			return (&((char *)str)[i]);
-		}
-		i++;
-	}
-	return (NULL);
+        i = 0;
+        while (i < ft_strlen((char *)str))
+        {
+                if (str[i] == c)
+                {
+                        return (&((char *)str)[i]);
+                }
+                i++;
+        }
+        if (c == '\0')
+                return (&((char *)str)[i]);
+        return (NULL);
 }
