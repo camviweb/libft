19b8a146320609ab3a24f1cf858f52a08611bd9b
Câm-Vi Nguyen <167216844+camviweb@users.noreply.github.com>
2024-12-11 16:25:14 +0100

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -16,12 +16,10 @@
 void    *ft_memchr(const void *str, int c, size_t n)
 {
         size_t                  i;
-        size_t                  len;
         unsigned char   *ptr;
 
         i = 0;
         ptr = (unsigned char *)str;
-        len = ft_strlen((char *)str) + 1;
         while (i < n)
         {
                 if (ptr[i] == (char)c)
